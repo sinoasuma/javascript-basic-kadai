ab6957ea393ee4097756c337de1dea3a25875a1e
sinoasuma <168266443+sinoasuma@users.noreply.github.com>
2024-06-11 15:57:05 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>課題005</title>
+</head>
+<body>
+ <p>
+    <?php
+    $number= 80+60+55+40+100+25+80+95+30+60;
+    echo $number/10;
+    ?>
+ </p>
+</body>
+</html>
+
